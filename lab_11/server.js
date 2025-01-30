4451f50c48a03e4d7f318de731c62d98e0efd380
owenrgu <o.meade@rgu.ac.uk>
2025-01-30 10:41:36 +0000

    Lab 11: Make form use post instead of get

--- lab_11/server.js
+++ lab_11/server.js
@@ -3,6 +3,7 @@ var knockknock = require("knock-knock-jokes");
 
 var app = express();
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 // route 1
 app.get("/", function(req, res) {
@@ -30,6 +31,12 @@ app.get("/add", function(req, res) {
 app.get("/getform", function(req, res) {
     var name = req.query.name;
     var quest = req.query.quest;
+    res.send("Hi " + name + "! I am sure you will " + quest);
+});
+
+app.post("/postform", function(req, res) {
+    var name = req.body.name;
+    var quest = req.body.quest;
     res.send("Hi " + name + "! I am sure you will " + quest);
 });
 
