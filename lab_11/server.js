0e13247fc9db38e76f638f90f31171e253bf430c
owenrgu <o.meade@rgu.ac.uk>
2025-01-30 10:37:16 +0000

    Lab 11: Add form route

--- lab_11/server.js
+++ lab_11/server.js
@@ -26,4 +26,11 @@ app.get("/add", function(req, res) {
     res.send("x + y =" + (x+y));
 });
 
+// form
+app.get("/getform", function(req, res) {
+    var name = req.query.name;
+    var quest = req.query.quest;
+    res.send("Hi " + name + "! I am sure you will " + quest);
+});
+
 app.listen(8080);
