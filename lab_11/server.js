49140f89c321ff434e1472eda26e7f152c370460
owenrgu <o.meade@rgu.ac.uk>
2025-01-30 10:06:17 +0000

    Lab 11: Routing with express

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var app = express();
 
+// route 1
 app.get("/", function(req, res) {
     res.send("Hello world! by express");
+});
+
+// route 2
+app.get("/test", function(req, res) {
+    res.send("this is route 2");
 });
 
 app.listen(8080);
