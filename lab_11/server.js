81c5439bba1425b5b277298479df5a01b2760cce
owenrgu <o.meade@rgu.ac.uk>
2025-01-30 10:09:10 +0000

    Lab 11: Joke route

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var knockknock = require("knock-knock-jokes");
 var app = express();
 
 // route 1
@@ -9,6 +10,11 @@ app.get("/", function(req, res) {
 // route 2
 app.get("/test", function(req, res) {
     res.send("this is route 2");
+});
+
+// route 3
+app.get("/joke", function(req, res) {
+    res.send(knockknock());
 });
 
 app.listen(8080);
