751849f7bfcc3eba28eec363dda587106cf08bc5
owenrgu <o.meade@rgu.ac.uk>
2025-03-13 09:45:02 +0000

    Lab 16: Setup socket & handle incoming messages

---
+++ lab_16/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+  });
+})
+
+http.listen(8080, function () {
+  console.log("listening on *:8080");
+});
